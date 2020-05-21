28a4316382245a6a8a32cde29663b4d01f3afa5f
Shinpei Hayashi <hayashi@c.titech.ac.jp>
2020-05-21 17:52:41 +0900

    Implement an exercise version (not working)

--- app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
@@ -1,14 +1,69 @@
 package jp.ac.titech.itpro.sdl.camera;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static int REQ_PHOTO = 1234;
+    private Bitmap photoImage = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button photoButton = findViewById(R.id.photo_button);
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                // TODO: You should setup appropriate parameters for the intent
+
+                PackageManager manager = getPackageManager();
+                List activities = manager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+                if (!activities.isEmpty()) {
+                    startActivityForResult(intent, REQ_PHOTO);
+                } else {
+                    Toast.makeText(MainActivity.this, R.string.toast_no_activities, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void showPhoto() {
+        if (photoImage == null) {
+            return;
+        }
+        ImageView photoView = findViewById(R.id.photo_view);
+        photoView.setImageBitmap(photoImage);
+    }
+
+    @Override
+    protected void onActivityResult(int reqCode, int resCode, Intent data) {
+        super.onActivityResult(reqCode, resCode, data);
+        switch (reqCode) {
+            case REQ_PHOTO:
+                if (resCode == RESULT_OK) {
+                    // TODO: You should implement the code that retrieve a bitmap image
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        showPhoto();
     }
 }
