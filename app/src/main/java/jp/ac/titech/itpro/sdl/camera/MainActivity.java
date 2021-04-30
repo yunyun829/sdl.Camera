a6616ba0822a5149ae2cc4300c215b21349b22eb
Shinpei Hayashi <hayashi@c.titech.ac.jp>
2021-04-30 15:23:04 +0900

    Use lambda

--- app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
@@ -1,10 +1,11 @@
 package jp.ac.titech.itpro.sdl.camera;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -24,19 +25,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button photoButton = findViewById(R.id.photo_button);
-        photoButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                // TODO: You should setup appropriate parameters for the intent
+        photoButton.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            // TODO: You should setup appropriate parameters for the intent
 
-                PackageManager manager = getPackageManager();
-                List activities = manager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
-                if (!activities.isEmpty()) {
-                    startActivityForResult(intent, REQ_PHOTO);
-                } else {
-                    Toast.makeText(MainActivity.this, R.string.toast_no_activities, Toast.LENGTH_LONG).show();
-                }
+            PackageManager manager = getPackageManager();
+            @SuppressLint("QueryPermissionsNeeded")
+            List<ResolveInfo> activities = manager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+            if (!activities.isEmpty()) {
+                startActivityForResult(intent, REQ_PHOTO);
+            } else {
+                Toast.makeText(MainActivity.this, R.string.toast_no_activities, Toast.LENGTH_LONG).show();
             }
         });
     }
